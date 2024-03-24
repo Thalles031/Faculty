708fcd0374761c50419764a6fef5dfa3857f23c6
Thalles Gabriel <161670635+Thalles031@users.noreply.github.com>
2024-03-24 10:20:04 -0300

    Add files via upload

---
+++ psc-lista-04-2024/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double numUm, numDois, numTres, numQuatro, numCinco, soma, media;
+        
+        System.out.println("Informe o primeiro número: ");
+        numUm = scanner.nextDouble();
+
+        System.out.println("Informe o segundo número: ");
+        numDois = scanner.nextDouble();
+
+        System.out.println("Informe o terceiro número: ");
+        numTres = scanner.nextDouble();
+
+        System.out.println("Informe o quarto número: ");
+        numQuatro = scanner.nextDouble();
+
+        System.out.println("Informe o quinto número: ");
+        numCinco = scanner.nextDouble();
+
+        soma = numUm + numDois + numTres + numQuatro + numCinco;
+
+        media = soma / 5;
+
+
+        System.out.print(String.format("A soma dos números é %s e a média é %s", soma, media));
+        scanner.close();
+    }
+}
