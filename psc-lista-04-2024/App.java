bc39937170fc45c5ee0052e002159b9a7b16f30f
Thalles Gabriel <161670635+Thalles031@users.noreply.github.com>
2024-03-24 00:55:52 -0300

    Add files via upload

---
+++ psc-lista-04-2024/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String nome = null;
+        String senha = null;
+
+        System.out.println("Informe o seu nome de usuario: ");
+        nome = input.nextLine();
+        System.out.println("Informe a sua senha: ");
+        senha = input.nextLine();
+
+        while (nome.equals(senha))
+        {
+            System.out.println("A sua senha nao pode ser igual ao seu nome de usuario, informe outros: ");
+            nome = input.nextLine();
+            senha = input.nextLine();
+        }
+
+        input.close();
+        
+    }
+
+}
