aabd199a3fd101a92ecd52d3857078b876fcbe1b
Thalles Gabriel <161670635+Thalles031@users.noreply.github.com>
2024-03-24 10:08:24 -0300

    Add files via upload

---
+++ psc-lista-04-2024/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class App {
+        public static void main(String[] args) {
+            Scanner imput= new Scanner(System.in);
+            double paisUm = 80000;
+            double paisDois = 200000;
+    
+            System.out.println(" Vamos calcular os anos necessários para que a população A ultrapasse ou iguale a B");
+        
+            int ano = 0;
+            for (double index = paisUm * 0.03; index < paisDois * 0.015; index++) {
+                ano++;
+    
+                System.out.print(ano + ", ");
+            }
+    
+            imput.close();
+        }
+    }
