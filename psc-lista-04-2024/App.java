b4ce22a609bf8222c9594383f98e8ea8f9706c55
Thalles Gabriel <161670635+Thalles031@users.noreply.github.com>
2024-03-24 10:22:43 -0300

    Add files via upload

---
+++ psc-lista-04-2024/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Valores ímpares:");
+for (int i = 0; i < 50; i++) {
+   if (i % 2 != 0) {
+       System.out.println(i);
+   }
+   
+}
+   }
+}
